33c772dd0fdd6b7d36f15c3c90bab0d4918e44f5
avivwei96 <76816214+avivwei96@users.noreply.github.com>
2025-01-09 21:40:56 +0200

    Implement lemmatization in IndexingTextOptimizer and update main.py to use it

--- components/IndexOptimizer/indexing_text_optimizer_interface.py
+++ components/IndexOptimizer/indexing_text_optimizer_interface.py
@@ -1,7 +1,9 @@
 from abc import ABC, abstractmethod
-from components.query import Query
+from query import Query
 from typing import List
-from components.web_text_unit import WebTextUnit
+from web_text_unit import WebTextUnit
+import trankit
+import torch
 
 class IndexingTextOptimizerInterface(ABC):
     @abstractmethod
@@ -9,6 +11,31 @@ class IndexingTextOptimizerInterface(ABC):
         pass
 
 class NoneIndexOptimizer(IndexingTextOptimizerInterface):
-
     def optimize_text(self, text: str) -> str:
         return text
+    
+class LemmatizerIndexOptimizer(IndexingTextOptimizerInterface):
+    def __init__(self):
+        self.pipeline = trankit.Pipeline("hebrew")
+        # check if gpu is available
+        if torch.cuda.is_available():
+            print("Using GPU to lemmatize")
+        else:
+            print("Using CPU to lemmatize")
+
+        
+    def optimize_text(self, text: str) -> str:
+        if not text:
+            return text
+        
+        # Perform lemmatization using Trankit
+        lemmatize_tokens = self.pipeline.lemmatize(text)
+
+        # Construct lemmatized text by iterating over tokens
+        lemmatized_text = " ".join(
+            token.get('lemma', token['text']) for sentence in lemmatize_tokens['sentences'] for token in sentence['tokens']
+        )
+    
+        return lemmatized_text
+
+
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from components.index_data_interface import Bm25Indexer
 from components.LlmAnswerRetriever.llm_answer_retriever_interface import EmptyAnswerRetrieverInterface
 from components.LlmAnswerRetriever.GeminiFreeTierAnswerRetriever import GeminiFreeTierAnswerRetriever
 from components.rag_results import RagResults
-from components.IndexOptimizer.indexing_text_optimizer_interface import  NoneIndexOptimizer
+from components.IndexOptimizer.indexing_text_optimizer_interface import  LemmatizerIndexOptimizer as Lema
 from components.rag import Rag
 
 use_small_data = False
@@ -26,7 +26,7 @@ def run_rag():
     queries = parse_queries_csv(eval_set_name)
     queries = queries[:15]
     pre_proccessor = WebDataPreProccessor(web_database_name)
-    index_optimizers = [NoneIndexOptimizer()]
+    index_optimizers = [Lema()]
     index_data_impl = Bm25Indexer()
     get_final_answers_retriever = EmptyAnswerRetrieverInterface()
 
