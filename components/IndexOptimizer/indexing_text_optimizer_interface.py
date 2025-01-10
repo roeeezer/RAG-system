5afff190a58954b848fd5fbb34da452c2053298e
avivwei96 <76816214+avivwei96@users.noreply.github.com>
2025-01-10 14:05:37 +0200

    Refactor LemmatizerIndexOptimizer by moving lemmatization logic to a separate function and enhancing document optimization process

--- components/IndexOptimizer/indexing_text_optimizer_interface.py
+++ components/IndexOptimizer/indexing_text_optimizer_interface.py
@@ -4,6 +4,7 @@ import torch
 from typing import List
 import re
 
+
 class IndexingTextOptimizerInterface(ABC):
     @abstractmethod
     def optimize_query(self, lst_text: List[str]) -> List[str]:
@@ -21,44 +22,6 @@ class NoneIndexOptimizer(IndexingTextOptimizerInterface):
     def optimize_document(self, lst_text: List[str]) -> List[str]:
         return lst_text
     
-class LemmatizerIndexOptimizer(IndexingTextOptimizerInterface):
-    def __init__(self):
-        self.pipeline = trankit.Pipeline("hebrew")
-        # check if gpu is available
-        if torch.cuda.is_available():
-            print("Using GPU to lemmatize")
-        else:
-            print("Using CPU to lemmatize")
 
-        
-    def optimize_document(self, lst_text: List[str]) -> List[str]:
-        if not lst_text:
-            return lst_text
-        
-        preprocessed_texts = []
-        for text in lst_text:
-            # Add spaces around punctuation and special characters
-            text = re.sub(r"([^\w\s])", r" \1 ", text)  # Add spaces around non-word characters
-            text = re.sub(r"\s+", " ", text)  # Normalize multiple spaces to a single space
-            # Remmove * : . , " ' from text
-            text = re.sub(r"[*:,\"']", "", text)
-            preprocessed_texts.append(text.strip().split())
-        # Perform lemmatization using Trankit
-
-        lemmatize_tokens = self.pipeline.lemmatize(preprocessed_texts)
-
-        # Construct lemmatized text by iterating over tokens
-
-        lemmatized_texts = [
-            " ".join(
-                token['text'] if token.get('lemma', "_") == "_" else token['lemma']
-                for token in sentence['tokens']
-            )
-            for sentence in lemmatize_tokens['sentences']
-        ]
-        return lemmatized_texts
-    
-    def optimize_query(self, lst_text: List[str]) -> List[str]:
-        return self.optimize_document(lst_text)
 
 
