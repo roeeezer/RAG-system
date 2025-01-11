7f5355f9874c98709fe94f39414dfb61f828b4b5
Roee Ezer <roeeezer2@gmail.com>
2025-01-11 17:37:38 +0200

    improved PrefixSuffixSplitterOptimizer signinficantly:     "recall": 0.89,     "mmr": 0.64,

--- components/IndexOptimizer/prefix_suffix_splitter_optimizer.py
+++ components/IndexOptimizer/prefix_suffix_splitter_optimizer.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import re
 
 # Add the project root to the Python path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
@@ -39,12 +40,23 @@ class PrefixSuffixSplitterOptimizer(IndexingTextOptimizerInterface):
         return self.optimize_queries(lst_text)
 
     def optimize_text(self, text: str) -> str:
-        text = text.split()
+        text = self.split_and_trim_hebrew_words(text)
         new_text = []
         for word in text:
             self.append_word(new_text, word)
             self.split_prefixes_and_suffixes(new_text, word)
         return " ".join(new_text)
+
+    def split_and_trim_hebrew_words(self, text):
+        # Split the text by whitespace or backslash
+        words = re.split(r'[\\\s]+', text)
+        # Regex pattern to match only Hebrew characters
+        hebrew_pattern = re.compile(r'^[\u0590-\u05FF]+$')
+        # Trim non-Hebrew characters from the sides of each word
+        trimmed_words = [re.sub(r'^[^\u0590-\u05FF]+|[^\u0590-\u05FF]+$', '', word) for word in words]
+        # Filter out empty strings and non-Hebrew words
+        filtered_words = [word for word in trimmed_words if word and hebrew_pattern.match(word)]
+        return filtered_words
 
     def split_prefixes_and_suffixes(self, new_text, word):
         self.split_prefixes(new_text, word)
@@ -101,4 +113,6 @@ def test():
     print(new_text)
 
 if __name__ == "__main__":
-    test()
+    o = PrefixSuffixSplitterOptimizer()
+    res = o.split_and_trim_hebrew_words("טיפולים לילדים עם הפרעה בשטף הדיבור (גמגום) (זכות). זה המשך, המשפט- עם0 סימנים- מוזרים")
+    print(res)
